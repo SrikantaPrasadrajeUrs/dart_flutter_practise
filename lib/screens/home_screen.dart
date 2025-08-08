f6408d3069fbd04953c2310ac11ded5853689848
SrikantaPrasadrajeUrs <srikantaprasadrajeurs456@gmail.com>
2025-08-08 23:00:56 +0530

    updated

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -9,20 +9,62 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  TextEditingController titleController = TextEditingController();
+  TextEditingController contentController = TextEditingController();
+
   final instance = FirebaseFirestore.instance;
   CollectionReference<Map<String, dynamic>> get notes => instance.collection("notes");
-  Stream<List> getNotes(){
 
+  Stream<List> getNotes(){
     // map -> Iterable - Modify each element
     return notes.snapshots().map(
             (snapshot)=>snapshot.docs.map((doc){
-              return doc.data();
+              return doc.data();// Map -> key:value
             }).toList()
     );
   }
 
+  @override
+  void initState() {
+    super.initState();
+  }
+
   void deleteNote(String noteId)async{
     await notes.doc(noteId).delete();
+  }
+
+  // flutter -> db
+  void addNote(String title, String content){
+
+  }
+
+  void showPop(){
+    showDialog(
+      context: context,
+      builder:(context)=>
+          AlertDialog(
+            title: const Text("Add note"),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: "Enter title"
+                  ),
+                  controller: titleController,
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                      hintText: "Enter content"
+                  ),
+                )
+              ],
+            ),
+          )
+    ).whenComplete((){
+      print(titleController.text);
+    });
+
   }
 
   @override
@@ -67,6 +109,13 @@ class _HomeScreenState extends State<HomeScreen> {
             }
             return SizedBox();
           }
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.orange,
+        onPressed: () {
+          showPop();
+        },
+        child: Icon(Icons.add, color: Colors.black.withValues(alpha: .5)),
       )
     );
   }
